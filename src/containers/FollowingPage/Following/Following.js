90b5553e95d142c22eb33c2c987983895694a5ee
kashif nehal <kashif4918@gmail.com>
2020-06-22 23:35:47 +0530

    made capture button only not it's function

--- src/containers/FollowingPage/Following/Following.js
+++ src/containers/FollowingPage/Following/Following.js
@@ -4,37 +4,51 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
+// import InputGroup from 'react-bootstrap/InputGroup'
+// import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+// import Form from 'react-bootstrap/Form'
 import classes from './Following.css'
 import WriteImage from '../../../assets/images/2.jpg'
+import AddMedia from './AddMedia/AddMedia'
+
+
+
 class Following extends Component {
+    state= {
+        mediaClicked:false
+    }
+    fileSelectHandler = event => {
+        console.log(event.target.files[0])
+    }
+
+    captureHandler = () => {
+        this.setState({mediaClicked:true})
+    }
+
     render () {
         return (
             <Auxiliary className={classes.Write}>
-                <Container className={classes.WriteContainer} fluid style={{backgroundColor:'white'}}>
+                <Container className={classes.WriteContainer} fluid style={{backgroundColor:'white',marginTop:'5px'}}>
                     <Row className={classes.WriteRow}>
-                        {/* <Col className={classes.WritePic} xs={2} style={{border:'red 1px solid',height:'150px'}}><Image src={WriteImage} roundedCircle style={{height:'50px', width:'50px'}}/></Col> */}
-                        <Col className={classes.WriteBox} xs={12}style={{border:'green 1px solid'}}>
+                        <Col className={classes.WritePic} xs={2} style={{border:'red 1px solid',height:'150px'}}><Image src={WriteImage} roundedCircle style={{height:'50px', width:'50px'}}/></Col>
+                        <Col className={classes.WriteBox} xs={10}style={{border:'green 1px solid'}}>
                             <Row>
-                                <Col xs={12}>
-                                    <InputGroup>
-                                        <InputGroup.Prepend style={{display:'flex',alignItems:'flex-start'}}>
-                                            <InputGroup.Text style={{backgroundColor:'white', border:'white'}}><Image src={WriteImage} roundedCircle style={{height:'50px', width:'50px'}}/></InputGroup.Text>
-                                        </InputGroup.Prepend>
-                                        <FormControl as="textarea" aria-label="With textarea" placeholder="Hello! I am Groot.." style={{height:'120px',border:'white'}}/>
-                                    </InputGroup>
+                                <Col xs={12} >
+                                    <textarea className={classes.text} rows="4" cols="40" placeholder="Hello! I am Groot..." style={{outline:'none',border:'none'}} ></textarea>
                                 </Col>
                             </Row>
                             <Row>
                                 <Col xs={6}></Col>
-                                <Col xs={1}><ion-icon name="image-outline"></ion-icon></Col>
-                                <Col xs={1}><ion-icon name="videocam-outline"></ion-icon></Col>
-                                <Col xs={1}><ion-icon name="camera-outline"></ion-icon></Col>
-                                <Col xs={3}><Button variant="primary" size="sm" >Post</Button></Col>
+                                <Col>
+                                    <label htmlFor="imageUpload"><ion-icon name="videocam-outline" size="large"></ion-icon></label>
+                                    <input type="file" id="imageUpload"onChange={this.fileSelectHandler} style={{display:'none'}}/>
+                                </Col>
+                                <Col>
+                                    <button onClick={this.captureHandler}>capture</button>
+                                </Col>
+                                <Col><Button variant="primary" size="sm" >Post</Button></Col>
                             </Row>
-                            
                         </Col>
                     </Row>
                 </Container>
